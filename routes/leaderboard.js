2c44cb1120bcb0bfe9b0ddc501a05a24a8c07c22
Rutger de Jong <rutgerdj2@gmail.com>
2021-11-23 16:46:55 +0100

    Add server code

---
+++ routes/leaderboard.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+var sql = require("mssql");
+
+var config = {
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  server: process.env.DBSERVER,
+  database: process.env.DBNAME,
+};
+
+
+router.get("/:leaderboardId/:year?/:day?", function (req, res, next) {
+  console.log(req.params);
+
+  sql.connect(config, function (err) {
+    if (err){
+      console.log(err);
+      res.sendStatus(500).send("Server error");
+      return;
+    }  
+    var request = new sql.Request();
+    request.input("leaderboardId", req.params.leaderboardId.slice(0, 36));
+    request.input("year", req.params.year);
+    request.input("day", req.params.day);
+    request.query(
+      `
+      SELECT year, day, username, startTime, starOne, starTwo FROM challenges 
+      INNER JOIN users on users.userid = challenges.userid
+      WHERE 
+        (year = @year or @year is null)
+        and
+        (day = @day or @day is null)
+        and 
+        (leaderboardId = @leaderboardId or @leaderboardId is null)
+    `,
+      function (err, recordset) {
+        if (err){
+          console.log(err);
+          res.sendStatus(500).send("Server error");
+          return;
+        }
+        res.json(recordset.recordsets[0]);
+      }
+    );
+  });
+});
+
+
+router.post("/:leaderboardId/:year/:day", function (req, res, next) {
+  
+  if (!req.params.year || !req.params.day || !req.body.userid){
+    console.log("watt?S")
+    res.sendStatus(400).send("Wrong data in request");
+    return;
+  }
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
+    var request = new sql.Request();
+    
+    request.input("lid", req.params.leaderboardId.slice(0, 36));
+    request.input("uid", req.body.userid.slice(0, 36));
+    request.input("yr", req.params.year);
+    request.input("dy", req.params.day);
+    request.input("sTime", req.body.startTime);
+    request.input("star1", req.body.starOne);
+    request.input("star2", req.body.starTwo);
+    
+    request.query(`
+      EXEC updateProgress
+        @leaderboardId = @lid,
+        @userid = @uid,
+        @year = @yr,
+        @day = @dy,
+        @startTime = @sTime,
+        @starOne = @star1,
+        @starTwo = @star2
+    `, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500).send("Server error");
+        return;
+      }
+      res.sendStatus(201);
+    });
+
+  });
+});
+
+module.exports = router;

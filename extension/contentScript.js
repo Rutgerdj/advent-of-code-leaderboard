266578fa525145ff51a25ea47bd93ab6d38b20b7
Rutger <rutgerdj2@gmail.com>
2021-11-27 19:38:02 +0100

    Content script now reports userdata (username, github page and profile pic)

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,7 +2,7 @@ const urlMatch = window.location.href.match(
   /adventofcode\.com\/(\d{4})(\/day\/(\d{1,2})(\/answer)?)?/
 );
 
-if (urlMatch[2]){
+if (urlMatch && urlMatch[2]){
     const year = urlMatch[1];
     const day = urlMatch[3];
 
@@ -21,5 +21,36 @@ if (urlMatch[2]){
         year: year,
         day: day,
         progress: progress
+    }, () => {});
+}
+
+if (window.location.href.match(/adventofcode\.com\/\d{4}\/settings/)){
+    let userdata = {
+        username: null,
+        githubPage: null,
+        profilePic: null
+    };
+    let settings = document.querySelector("#settings");
+    let url = settings.querySelector("#display_url");
+    if (url.checked){
+        userdata.githubPage = url.parentElement.querySelector("span").innerHTML.match(/https:\/\/github.com\/.+/)[0];
+    }
+    
+    settings.querySelectorAll("input[type=radio]").forEach((radio) => {
+        if (radio.checked){
+            let sp = radio.parentElement.querySelector("span");
+            userdata.username = sp.innerText;
+
+            let img = sp.querySelector("img");
+            if (img){
+                userdata.profilePic = img.src;
+            }
+
+        }
     });
+
+    chrome.runtime.sendMessage({
+        command: "updateUserdata",
+        userdata: userdata
+    }, () => {});
 }
